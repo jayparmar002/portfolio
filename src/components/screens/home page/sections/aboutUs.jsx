3e6b83ca9955c562245062feb2f12ae5b8e5d40b
developerdesigner18 <developer.designer18@gmail.com>
2025-09-17 10:01:14 +0530

    remove code

--- src/components/screens/home page/sections/aboutUs.jsx
+++ src/components/screens/home page/sections/aboutUs.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Button from '../../../ui/btn'
-import SplitText from '../../../ui/SplitText';
 import { Link } from 'react-router-dom';
 import Heading from '../../../ui/heading';
 
